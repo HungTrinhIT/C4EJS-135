087b636937b82a2ef531a5c425dbcc14da61d99d
HungTrinhJH <hung.trinh@journeyh.io>
2023-02-12 21:48:17 +0700

    complete lesson 01

--- homework-js-dom/homework-01/index.js
+++ homework-js-dom/homework-01/index.js
@@ -1,5 +1,25 @@
-const bulboffImage = document.querySelector(".bulboff-container img");
+// Cách 1
+// const image = document.querySelector(".bulboff-container img");
 
-function turnOff() {}
+// function turnOff() {
+//   image.src = "./images/pic_bulboff.gif";
+// }
 
-function turnOn() {}
+// function turnOn() {
+//   image.src = "./images/pic_bulbon.gif";
+// }
+
+// Cách 2
+function toggleBubble(status) {
+  const image = document.querySelector(".bulboff-container img");
+  switch (status) {
+    case "off":
+      image.src = "./images/pic_bulboff.gif";
+      return;
+    case "on":
+      image.src = "./images/pic_bulbon.gif";
+      return;
+    default:
+      return;
+  }
+}
